8da5bb17073f810cb642288f1aff34c802fdb7df
vincent <vin.beck24@gmail.com>
2026-08-05 20:55:35 +0200

    create menu state handler

---
+++ Assets/Scripts/gui/MenuStateHandler.cs
@@ -0,0 +1,79 @@
+ using System;
+using ScenePersistence;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+namespace gui
+{
+    public class MenuStateHandler : MonoBehaviour
+    {
+        public InputActionReference pauseKey;
+    
+        public MenuState menuState;
+        
+        [Header("UI Documents")]
+        public UIDocument settingsUI;
+        public UIDocument otherUI;
+
+        private void Start()
+        {
+            menuState = SceneManager.GetActiveScene().name == "MainMenu" ? MenuState.MainMenu : MenuState.Game;
+        }
+    
+        private void Update()
+        {
+            if (!pauseKey.action.triggered) return;
+            switch (menuState)
+            {
+                case MenuState.MainMenu:
+                    break;
+                case MenuState.Game:
+                    // open pause menu
+                    break;
+                case MenuState.SettingsMenu:
+                    CloseSettingsMenu();
+                    menuState = SceneManager.GetActiveScene().name == "MainMenu" ? MenuState.MainMenu : MenuState.PauseMenu;
+                    break;
+                case MenuState.PauseMenu:
+                    // Resume
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        
+        }
+        
+        public void OpenSettingsMenu()
+        {
+            otherUI.rootVisualElement.visible = false;
+            settingsUI.rootVisualElement.visible = true;
+            menuState = MenuState.SettingsMenu;
+        }
+        
+        public void CloseSettingsMenu()
+        {
+            otherUI.rootVisualElement.visible = true;
+            settingsUI.rootVisualElement.visible = false;
+        }
+        
+        public void StartGame()
+        {
+            SceneChanger.ChangeSceneNow("Game");
+        }
+        
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
+    }
+
+    public enum MenuState
+    {
+        MainMenu,
+        Game,
+        PauseMenu,
+        SettingsMenu
+    }
+}
