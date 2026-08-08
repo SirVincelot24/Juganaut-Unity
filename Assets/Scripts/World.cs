69a1e0709dcb3c3acba6d29b23a576af60e43237
vincent <vin.beck24@gmail.com>
2026-08-08 18:36:26 +0200

    create world and worldbuilder scripts

---
+++ Assets/Scripts/World.cs
@@ -0,0 +1,91 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using JetBrains.Annotations;
+using space;
+using UnityEngine;
+using WorldItems;
+using Object = UnityEngine.Object;
+
+public class World
+{
+    private readonly List<List<WorldItemType>> _fields;
+
+    public readonly IEnumerable<int> ValidXRange;
+    public readonly IEnumerable<int> ValidYRange;
+
+    public readonly int Width;
+    public readonly int Height;
+    
+    public World(int width, int height)
+    {
+        Width = width;
+        Height = height;
+        _fields = Enumerable.Range(0, Height)
+            .Select(_ => new List<WorldItemType>(Enumerable.Repeat(WorldItemType.Dirt, Width)))
+            .ToList();
+        ValidXRange = Enumerable.Range(0, Width);
+        ValidYRange = Enumerable.Range(0, Height);
+    }
+    
+    public void SpawnItems()
+    {
+        var worldItemList = Resources.LoadAll<WorldItem>("Scriptable Objects");
+        foreach (var item in _fields.SelectMany(row => row))
+        {
+            Object.Instantiate(worldItemList.FirstOrDefault(i => i.type == item)?.prefab, new Vector3(1, 1),  Quaternion.identity);
+        }
+    }
+
+    public bool IsValid(Coord coord)
+    {
+        return ValidXRange.Contains(coord.X) && ValidYRange.Contains(coord.Y);
+    }
+
+    public void SetField(Coord coord, WorldItemType item)
+    {
+        _fields[coord.Y][coord.X] = item;
+    }
+
+    public WorldItemType GetField(Coord coord)
+    {
+        return _fields[coord.Y][coord.X];
+    }
+    
+    [CanBeNull]
+    public Coord Find(Func<WorldItemType, bool> condition)
+    {
+        Coord result = null;
+        _foreachCoord(coord =>
+        {
+            if (!condition(GetField(coord))) return;
+            result = coord;
+        });
+        return result;
+    }
+    
+    public List<Coord> FindAll(Func<WorldItemType, bool> condition)
+    {
+        var result = new List<Coord>();
+        _foreachCoord(coord =>
+        {
+            if (condition(GetField(coord)))
+            {
+                result.Add(coord);
+            }
+        });
+        return result;
+    }
+
+    private void _foreachCoord(Action<Coord> block)
+    {
+        foreach (var x in ValidXRange)
+        {
+            foreach (var y in ValidYRange)
+            {
+                var c = new Coord(x, y);
+                block(c);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/setup/WorldBuilder.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using space;
+using UnityEngine;
+using WorldItems;
+using Random = UnityEngine.Random;
+
+namespace setup
+{
+    public static class WorldBuilder
+    {
+        public static World CreateWorld(
+            int width, int height,
+            Vector2 diamondCountRange,
+            Vector2 monsterCountRange,
+            Vector2 bombCountRange,
+            Vector2 rockCountRange,
+            Coord playerCoord
+            )
+        {
+            var world = new World(width, height);
+            // create items
+            CreateItems(world, rockCountRange, playerCoord, () => WorldItemType.Rock);
+            CreateItems(world, monsterCountRange, playerCoord, () => WorldItemType.Monster);
+            CreateItems(world, bombCountRange, playerCoord, () => WorldItemType.Bomb);
+            CreateItems(world, diamondCountRange, playerCoord, () => WorldItemType.Diamond);
+
+            world.SetField(playerCoord, WorldItemType.Player);
+            List<Coord> nextToPlayer = new()
+            {
+                playerCoord.Move(Direction.Up),
+                playerCoord.Move(Direction.Down),
+                playerCoord.Move(Direction.Right),
+                playerCoord.Move(Direction.Left),
+            };
+            foreach (var coord in nextToPlayer)
+            {
+                switch (world.GetField(coord))
+                {
+                    case WorldItemType.Diamond:
+                        break;
+                    default:
+                        world.SetField(coord, WorldItemType.Dirt);
+                        break;
+                }
+            }
+            return world;
+        }
+
+        private static Coord GetValidCoord(World world, Coord playerCoord)
+        {
+            var xRange = world.ValidXRange.Where(i => i != playerCoord.X);
+            var yRange = world.ValidYRange.Where(i => i != playerCoord.Y);
+            var xIndex = Random.Range(0, world.Width - 1);
+            var yIndex = Random.Range(0, world.Height - 1);
+            return new Coord(xRange.ElementAt(xIndex), yRange.ElementAt(yIndex));
+        }
+
+        private static void CreateItems(World world, Vector2 itemCountRange, Coord playerCoord,
+            Func<WorldItemType> itemFactory)
+        {
+            var itemCount = Random.Range(itemCountRange.x, itemCountRange.y);
+            for (var i = 0; i < itemCount; i++)
+            {
+                world.SetField(GetValidCoord(world, playerCoord), itemFactory());
+            }
+        }
+    }
+}
